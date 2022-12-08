bd3ccfaf8dd5a3ec572136e197d01edbeefe3327
bgarcia95 <bgarcia.95@outlook.com>
2022-12-08 01:23:03 -0600

    use yargs and notes functions

--- app.js
+++ app.js
@@ -27,23 +27,105 @@
 // const validator = require('validator');
 
 const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const getNotes = require('./notes.js');
-const msg = getNotes();
+const msg = notes.getNotes();
 
-console.log(msg);
-
-console.log(chalk.green.bold.inverse('Success!'));
+// console.log(msg);
 
 // console.log(validator.isURL('https/mead.io'));
 
 // Challenge: Use the chalk library in your project
-//
+//  
 // 1. Install version 2.4.1 of chalk
 // 2. Load chalk into app.js
 // 3. Use it to print the string "Success!" to the console in green
 // 4. Test your work
 
 // Bonus: Use docs to mess around with other styles. Make text bold and inversed.
+const greenMsg = chalk.green.bold.inverse('Success!');
+// console.log(greenMsg);
+
+// argv: argument vector -> array with arguments provided
+// console.log(process.argv[2]);
+
+// Customize yargs version
+yargs.version('1.1.0');
+
+// add, remove, read, list
+
+// Challenge: Add an option to yargs
+//
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    }
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body)
+  }
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Removing a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    }
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
+  }
+});
+
+// Challenge: Add two new commands
+//
+// 1. Setup command to support "list" command (print placeholder message for now)
+// 2. Setup command to support "read" command (print placeholder message for now)
+// 3. Test your work by running both commands to ensure correct output
+
+// create list command
+yargs.command({
+  command: 'list',
+  describe: 'Listing out all the notes',
+  handler: function () {
+    console.log('Listing out all the notes');
+  }
+});
+
+// create read command
+yargs.command({
+  command: 'read',
+  describe: 'Reading a note',
+  handler: function () {
+    console.log('Reading a note');
+  }
+});
+
+yargs.parse();
+
+// console.log(yargs.argv);
+
+
 
 
