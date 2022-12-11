c480b3e3ff95c95dc96870f350981cde2c9a7d98
bgarcia95 <bgarcia.95@outlook.com>
2022-12-11 11:30:42 -0600

    remove dead code

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-// const fs = require('fs');
-
-// fs.writeFileSync('notes.txt', 'My name is Brayan.');
-// fs.appendFileSync('notes.txt', ` I'm 27 years old.`)
-
-
-// Challenge
-
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
-
-// const add = require('./utils');
-
-// const sum = add(4, -2);
-
-// console.log(sum);
-
-//
-// Challenge: Define and use a function in a new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that retunrs "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the fuinction printing the message to console
-
 // const validator = require('validator');
 
 const chalk = require('chalk');
